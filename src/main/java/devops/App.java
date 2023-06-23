05d1f596a821502983f7772ee05911c72793e65e
Ketki Patankar <kpatanka@I-FLEX.COM>
2023-06-23 12:55:57 +0530

    commit

---
+++ src/main/java/devops/App.java
@@ -0,0 +1,5 @@
+public class App {
+   public void printMessage(String message){
+      System.out.println(message);
+   }
+}
---
+++ src/test/java/devops/AppTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+
+public class AppTest {
+   @Test
+   public void printing( ){
+      App hello = new App();
+      hello.printMessage("Hello World!");
+   }
+}
